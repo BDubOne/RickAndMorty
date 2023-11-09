e5da1d17ced135681908ae20211fe89cf4f7d39a
BDubOne <bryan.t.bartell@gmail.com>
2023-11-08 18:08:21 -0600

    idk

--- src/components/SeasonalCarousel.jsx
+++ src/components/SeasonalCarousel.jsx
@@ -4,21 +4,23 @@ import PropTypes from 'prop-types';
 
 function SeasonalCarousel({ episodes}) {
   return (
-    <Carousel>
+    <div>
+    <Carousel >
         {episodes.map((episode, idx) => (
             <Carousel.Item key={idx}>
                 <img className="d-block w-80 m-10"
                 src={episode.backgroundImage}
                 alt={episode.title}
                 />
-                <Carousel.Caption>
-                    <h3>{episode.title}</h3>
-                    <p>Release Date: {episode.releaseDate}</p>
-                    <p>Episode Number: {episode.episodeNumber}</p>                        
+                <Carousel.Caption class="Carousel-text" >
+                    <h3 class="Carousel-text">{episode.title}</h3>
+                    <p class="Carousel-text">Release Date: {episode.releaseDate}</p>
+                    <p class="Carousel-text">Episode Number: {episode.episodeNumber}</p>                        
                 </Carousel.Caption>
             </Carousel.Item>
         ))}
     </Carousel>
+    </div>
 )
 }
 
