9c4389a5d4aeb8d9a958878425c50339345448b3
BDubOne <bryan.t.bartell@gmail.com>
2023-11-08 18:28:58 -0600

    done for now

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,11 @@
 const HomePage = () =>{
     return (
         <>
-        <div className="min-h-screen bg-black flex flex-col items-center justify-center relative">
-          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-            <img className="max-w-full max-h-50" src="./src/assets/rickymort.svg" alt="Rick and Morty" />
-          </div>
+        <div id="homepage" className="min-h-screen bg-black flex flex-col items-center justify-center relative">
+        <h1>Rick and Morty</h1>
+        <p>Just watch it. It's good and stuff. If you check out some of the pages on here, you'll really like it. Rick, put that down! I am doing a good job. You just keep pestering me, ya know? You're bullying me an I just don't think it's. This thing is still on. How do I? </p>
+        <br/>
+        <p>*Bhuuuurp* Give it here</p>
         </div>
   
         </>
